e10d09cbd7478dc304af1d04f49b92139e288ce3
Álvaro Arranz <aarranz@ficodes.com>
2021-01-26 16:00:57 +0100

    Fix debug task

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,6 +83,7 @@ module.exports = function (grunt) {
             },
             librarydebug: {
                 options: {
+                    browsers: ["Chrome"],
                     singleRun: false
                 }
             },
